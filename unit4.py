18896a96bc7dc56f9fd2cb5f74c9c3dbde23c06a
Kumarvalavala66 <prasannakumarvalavala@gmail.com>
2025-10-31 23:58:32 +0530

    Resolve merge conflicts

--- unit4.py
+++ unit4.py
@@ -1,84 +1,84 @@
 import math
-# # Write a program to sort words in a file and put them in another file. The output file should have only lower-case words, so any upper-case words from source must be lowered.
-# def main():
-#     file = "normal.txt"
-#     with open(file,'r') as f:
-#         content = f.read()
-#     words = content.split()
-#     words = [word.lower() for word in words]
-#     words = " ".join(words)
-#
-#     with open('output.txt','w') as f:
-#         f.write(words)
-# if __name__ == '__main__':
-#     main()
+# Write a program to sort words in a file and put them in another file. The output file should have only lower-case words, so any upper-case words from source must be lowered.
+def main():
+    file = "normal.txt"
+    with open(file,'r') as f:
+        content = f.read()
+    words = content.split()
+    words = [word.lower() for word in words]
+    words = " ".join(words)
 
-# #  2. Python program to print each line of a file in reverse order.
-# def main():
-#     file = "file.txt"
-#     with open(file ,'r') as f:
-#         content= f.readlines()
-#         line_content = content
-#     with open(file,'w') as f:
-#         for line in line_content:
-#             f.write(line[::-1])
-#
-#
-# if __name__ == '__main__':
-#     main()
+    with open('output.txt','w') as f:
+        f.write(words)
+if __name__ == '__main__':
+    main()
 
-# #  3.Python program to compute the number of characters, words and lines in a file.
-# def main():
-#     file = "file.txt"
-#     with open(file,'r') as f:
-#         lines = f.readlines()
-#     lines_count = len(lines)
-#     for line in lines:
-#         words_count = len(line.split())
-#         letters_count = len(line)
-#
-#
-#     print("The number of lines in the file is:", lines_count)
-#     print("The number of words in the file is:", words_count)
-#     print("The number of characters in the file is:", letters_count)
-#
-#
-#
-# if __name__ == '__main__':
-#     main()
+#  2. Python program to print each line of a file in reverse order.
+def main():
+    file = "file.txt"
+    with open(file ,'r') as f:
+        content= f.readlines()
+        line_content = content
+    with open(file,'w') as f:
+        for line in line_content:
+            f.write(line[::-1])
 
 
-# # 4. Write a program to create, display, append, insert and reverse the order of the items in the array.
-# def main():
-#     my_list = [1, 2, 3,4,5,6,7]
-#     print(my_list)
-#     num = int(input('Enter a number to append : '))
-#     my_list.append(num)
-#     print("After appending ",num," in to list ",my_list)
-#     num1 = int(input('Enter a number to insert at 3rd idx: '))
-#     my_list.insert(3,num1)
-#     print("After inserting ",num1," in to list ",my_list)
-#     my_list.reverse()
-#     print("the reverse order is ",my_list)
-#
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
+
+#  3.Python program to compute the number of characters, words and lines in a file.
+def main():
+    file = "file.txt"
+    with open(file,'r') as f:
+        lines = f.readlines()
+    lines_count = len(lines)
+    for line in lines:
+        words_count = len(line.split())
+        letters_count = len(line)
 
 
-# # Write a program to add, transpose and multiply two matrices.
-# import numpy as np
-# def main():
-#     arr = np.random.randint(1,10 , size=(3,3))
-#     print(arr)
-#     arr1 = np.random.randint(1,10 , size=(3,3))
-#     print(arr1)
-#     print("the sum of both arr1 and arr2 is \n", arr + arr1)
-#     print("the multiplication  of both arr1 and arr2 is \n", arr @ arr1)
-#     print("the transpose of arr1  is \n", arr.T,"\n and  arr 2 is \n",arr1.T )
-#
-#
-# if __name__ == '__main__':
-#     main()
+    print("The number of lines in the file is:", lines_count)
+    print("The number of words in the file is:", words_count)
+    print("The number of characters in the file is:", letters_count)
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+# 4. Write a program to create, display, append, insert and reverse the order of the items in the array.
+def main():
+    my_list = [1, 2, 3,4,5,6,7]
+    print(my_list)
+    num = int(input('Enter a number to append : '))
+    my_list.append(num)
+    print("After appending ",num," in to list ",my_list)
+    num1 = int(input('Enter a number to insert at 3rd idx: '))
+    my_list.insert(3,num1)
+    print("After inserting ",num1," in to list ",my_list)
+    my_list.reverse()
+    print("the reverse order is ",my_list)
+
+if __name__ == '__main__':
+    main()
+
+
+# Write a program to add, transpose and multiply two matrices.
+import numpy as np
+def main():
+    arr = np.random.randint(1,10 , size=(3,3))
+    print(arr)
+    arr1 = np.random.randint(1,10 , size=(3,3))
+    print(arr1)
+    print("the sum of both arr1 and arr2 is \n", arr + arr1)
+    print("the multiplication  of both arr1 and arr2 is \n", arr @ arr1)
+    print("the transpose of arr1  is \n", arr.T,"\n and  arr 2 is \n",arr1.T )
+
+
+if __name__ == '__main__':
+    main()
 
 
 
